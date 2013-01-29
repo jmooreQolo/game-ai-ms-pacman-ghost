ac17d02b66aef19eb7a996eeaa07b41acba3aeb4
jmoore61gt <jmoore61gt@gmail.com>
2013-01-29 05:26:37 +0000

    Added the project for Storage on Google Code

---
+++ GameAI A1-Pacman/src/game/entries/ghosts/GhostFrightenedState.java
@@ -0,0 +1,12 @@
+package game.entries.ghosts;
+
+import game.core.Game;
+
+public class GhostFrightenedState implements GhostAI {
+
+	@Override
+	public int[] execute(int ghostType, Game game, long timeDue) {
+		return null;
+	}
+
+}
